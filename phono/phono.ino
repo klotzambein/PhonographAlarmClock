6611c5eaf92a9a36ecd29823ae0a6ef04a3cd2ef
Robin Kock <robin@kock-hamburg.de>
2017-11-05 15:44:40 +0100

    removing files

--- phono/phono.ino
+++ phono/phono.ino
@@ -3,7 +3,6 @@
 #import "servo.h"
 #import "clock.h"
 #import "text.h"
-#import "files.h"
 
 #define TIMER_INTERVAL 20000 // aka 50 hz
 #define PIN_SERVO_UD 9
@@ -17,22 +16,10 @@ void setup()
     Wire.begin(100);
     Serial.begin(9600);
 
-    pinMode(13, OUTPUT);
-    
     Timer1.initialize(TIMER_INTERVAL);
     Timer1.attachInterrupt(tick);
     Timer1.pwm(PIN_SERVO_UD, 0);
     Timer1.pwm(PIN_SERVO_LR, 0);
-
-    filePages[7] = {0x1883,
-                    0x0000,
-                    0x0000,
-                    0x0008};
-
-    char test[30] = "                             ";
-    sprintFileAddress(&test[0], 7, 30);
-    Serial.println(test);
-    findFile("home");
 
     Time tim = {0x30, 0x25, 0x17, 0x1, 0x20, 0x02, 0x17};
     settupClock(true, &tim, false, false, 0);
