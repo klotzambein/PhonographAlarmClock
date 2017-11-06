80dda37a75b5f2ebdb7a9e47b2e61aeed859b2c3
Robin Kock <robin@kock-hamburg.de>
2017-11-06 16:16:43 +0100

    Wifi credentials

---
+++ phono/wifi.h
@@ -0,0 +1,11 @@
+#ifndef WIFI_H
+#define WIFI_H
+
+#import "Arduino.h"
+
+//Create file access.h with these definitions:
+//#define WIFI_SSID "MyWiFi"
+//#define WIFI_PASS "Password"
+#import "access.h"
+
+#endif
